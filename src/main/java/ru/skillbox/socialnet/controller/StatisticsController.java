a68e8da2709b519b873333012963add0c05c3eeb
Sergey Kotov <kssp@list.ru>
2023-10-02 17:51:25 +0300

    feat: Statistics initial

---
+++ src/main/java/ru/skillbox/socialnet/controller/StatisticsController.java
@@ -0,0 +1,11 @@
+package ru.skillbox.socialnet.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/statistics")
+@RequiredArgsConstructor
+public class StatisticsController {
+}
---
+++ src/main/java/ru/skillbox/socialnet/service/StatisticsService.java
@@ -0,0 +1,9 @@
+package ru.skillbox.socialnet.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StatisticsService {
+}
