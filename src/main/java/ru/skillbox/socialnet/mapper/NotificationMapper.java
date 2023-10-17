9413490aab21818dbdc5409cac605d420be99109
KonstantinLi <linenkokonstantin@gmail.com>
2023-10-17 19:02:28 +0300

    fix: notification mappers injection

--- src/main/java/ru/skillbox/socialnet/mapper/NotificationMapper.java
+++ src/main/java/ru/skillbox/socialnet/mapper/NotificationMapper.java
@@ -3,7 +3,6 @@ package ru.skillbox.socialnet.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
-import org.mapstruct.factory.Mappers;
 import ru.skillbox.socialnet.dto.response.NotificationRs;
 import ru.skillbox.socialnet.entity.other.Notification;
 
@@ -14,8 +13,6 @@ import ru.skillbox.socialnet.entity.other.Notification;
         }
 )
 public interface NotificationMapper {
-    NotificationMapper INSTANCE = Mappers.getMapper(NotificationMapper.class);
-
     @Mapping(target = "info", source = "notification.contact")
     @Mapping(target = "entityAuthor", source = "notification.person")
     NotificationRs notificationToNotificationRs(Notification notification);
